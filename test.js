087e316ec456ef8219465fc9d4b066978a49bc60
Andy Barton <ajb31@outlook.com>
2018-05-16 03:38:27 +0100

    Final state of files for exercise 3

--- test.js
+++ test.js
@@ -1,18 +1,18 @@
-$(document).ready(function() {
-	
-	var sbHidden = false;
-	
-	$('.sidebar-sh').on('click', function(e) {
-		e.preventDefault();
-		
-		if (sbHidden) {
-			$('#content').animate({'width' : '70%'}, 1000);
-			$('#sidebar').fadeIn("slow");
-			sbHidden = false;
-		} else {
-			$('#sidebar').fadeOut("slow");
-			$('#content').animate({'width' : '100%'}, 1000);
-			sbHidden = true;
-		}
-	})
-})
+ $(document).ready(function() {
+	 $("#sidebar").toggle(function(e) {
+		e.preventDefault(); 
+		$("#sidebar-right").stop().animate({
+			width: "0"
+		}, 500);
+		$("#main").stop().animate({
+			width: "100%"
+		}, 500);
+	}, function() {
+		$("#sidebar-right").stop().animate({
+			width: "30%"
+		}, 500);
+		$("#main").stop().animate({
+			width: "70%"
+		}, 500);
+	});
+ });	 
